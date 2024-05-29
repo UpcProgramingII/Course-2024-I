b1d8172b4f22a1a0ce8021f65739715680b83ef6
UpcProgramingII <142351191+UpcProgramingII@users.noreply.github.com>
2024-05-29 06:45:44 -0500

    Add files via upload

---
+++ Unidad 5. Gui/ProyectoAlojamientoGUIFx/src/main/java/co/edu/unicesar/proyectoalojamientoguifx/SecondaryController.java
@@ -0,0 +1,12 @@
+package co.edu.unicesar.proyectoalojamientoguifx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
